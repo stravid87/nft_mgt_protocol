75d3ba35ae35581cfe2323295e2ced85878f6db8
Hermann EHO <hermannleboss@gmail.com>
2023-03-15 18:10:45 +0000

    feat: ownable first version

--- contracts/CryptoTip.sol
+++ contracts/CryptoTip.sol
@@ -2,7 +2,31 @@
 pragma solidity ^0.8.0;
 
 contract CryptoTip {
-    constructor(){
+    address owner;
+    //
+    //    struct Team {
+    //        string name;
+    //        address owner;
+    //        address [] members;
+    //    }
 
+    mapping(address => address []) public userTeam;
+
+    constructor(){
+        owner = msg.sender;
     }
+
+    function addTeamMember(address member) public {
+        userTeam[msg].push(member);
+    }
+
+    function getUserTeam() public view returns (address []) {
+        return userTeam[msg];
+    }
+
+        function sendTips() public payable {
+            address [] myTeam = userTeam[msg];
+            uint amount = msg.value / myTeam.length();
+            uint remain = msg.value - amount * myTeam.length();
+        }
 }
