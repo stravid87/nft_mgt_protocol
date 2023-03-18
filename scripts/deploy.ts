a8cac9095dabe5d1d96844c446321a4a2491b795
Hermann EHO <hermannleboss@gmail.com>
2023-03-18 15:31:25 +0000

    build: deploy function for crypto tip

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,20 @@
-import { ethers } from "hardhat";
+import {ethers, upgrades} from "hardhat";
 
 async function main() {
 
-  const CryptoTip = await ethers.getContractFactory("CryptoTip");
-  const cryptoTip = await CryptoTip.deploy();
+    const CryptoTip = await ethers.getContractFactory("CryptoTip");
+    const cryptoTip = await upgrades.deployProxy(CryptoTip, {initializer: 'initialize'});
 
-  await cryptoTip.deployed();
+    await cryptoTip.deployed();
 
-  console.log(
-    `CryptoTip is deployed to ${cryptoTip.address}`
-  );
+    console.log(
+        `CryptoTip is deployed to ${cryptoTip.address}`
+    );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
